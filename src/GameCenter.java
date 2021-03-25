2a20e25165c71bace8505620840e728f9bdff159
J1NWAN <weamicron@gmail.com>
2021-03-26 00:07:32 +0900

    미니게임(가위바위보, 업다운) 추가

--- src/GameCenter.java
+++ src/GameCenter.java
@@ -1,7 +1,74 @@
+import java.util.Scanner;
 
-public class GameCenter implements Controller{
-	
+public class GameCenter implements Controller {
+	public GameCenter(String user) {
+		System.out.println(user + " Login ~\n");
+	}
+
 	public void up_Down() {
+		Scanner sc = new Scanner(System.in);
+		int computer = (int) (Math.random() * 50 + 1);
+		int player = 0;
+		int count = 0;
+
+		System.out.println("\n# Up and Down Game #\n");
+		for (int i = 10; i >= 1; i--) {
+			System.out.println("You have " + i + " chances.");
+			System.out.print("> ");
+			player = sc.nextInt();
+
+			if (computer > player) {
+				System.out.println("Up !!\n");
+			} else if (computer < player) {
+				System.out.println("Down ~\n");
+			} else {
+				System.out.println("\nYour Victory :)");
+				break;
+			}
+
+			count++;
+		}
+
+		if (count == 10) {
+			System.out.println("\nYou Lose.. :(");
+		}
+	}
+
+	public void rock_Paper_Scissors() {
+		System.out.println("\n# Rock Paper Scissors Game #\n");
+		Scanner sc = new Scanner(System.in);
 		
+		int player = 0;
+		int coin = 1;
+
+		// 0 - 묵 1 - 찌 2 - 빠
+		while (true) {
+			System.out.print("(0) Rock  (1) Scissors  (2) Paper\nSelect > ");
+			int computer = (int) (Math.random() * 3);
+			player = sc.nextInt();
+
+			if (coin != 2) {
+				if (player == computer) {
+					System.out.println("\n@Draw :o\n");
+					continue;
+				} else if ((player == 0 && computer == 2) || (player == 1 && computer == 0)
+						|| (player == 2 && computer == 1)) {
+					System.out.println("\n@You Lose.. :(\n");
+					System.out.print("Continue ? 1. Yes, 2. No ");
+					coin = sc.nextInt();
+				} else {
+					System.out.println("\n@Your Victory :)\n");
+					System.out.print("Continue ? 1. Yes, 2. No ");
+					coin = sc.nextInt();
+					
+				}
+				System.out.println("");
+			}
+			
+			if(coin == 2) {
+				System.out.println("# Game Over #");
+				break;
+			}
+		}
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 
 public class Player {
-	public void main(String[] args) {
-		
+	public static void main(String[] args) {
+		GameCenter player = new GameCenter("J's");
+		int select = 0;
+		int count = 1;
+
+		Scanner sc = new Scanner(System.in);
+
+		while (count == 1) {
+			System.out.println("\n1. Up and Down Game");
+			System.out.println("2. Rock Paper Scissors Game");
+			System.out.print("3. Exit\n\nprompt > ");
+			
+			select = sc.nextInt();
+			switch (select) {
+			case 1:
+				player.up_Down();
+				break;
+
+			case 2:
+				player.rock_Paper_Scissors();
+				break;
+
+			case 3:
+				System.out.println("\nExit\n");
+				count = 0;
+				break;
+			default:
+				System.out.println("\n@@ Invalid Input @@\n");
+			}
+		}
 	}
 }
